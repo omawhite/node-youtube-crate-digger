6d6cbef05635d4d79de92cb3bf3794e625b348ef
Omar White <omawhite@indiana.edu>
2023-06-27 22:22:00 -0400

    does some refactoring

--- src/index.ts
+++ src/index.ts
@@ -3,17 +3,27 @@ import { spawn } from 'child_process';
 console.log('lets dig some crates!');
 
 console.log('setting things up');
-const playlistUrl =
-  'https://www.youtube.com/playlist?list=PLvym70oeEfS7HN_xqxC1E7zjucWYBOnfK';
-
-const youtube_dl_options = [
+const isProduction = process.env.NODE_ENV === 'production';
+console.log(`isProduction:`, isProduction);
+const batchFileLocation = 'batchFile.txt';
+const args = [
   '--download-archive',
   'download-archive.txt',
-  `-o downloads/%(playlist)s/%(playlist_index)s - %(title)s.%(ext)s`
+  '-o',
+  'downloads/%(playlist)s/%(playlist_index)s - %(title)s.%(ext)s',
+  '--extract-audio',
+  '--ignore-errors',
+  '--audio-format',
+  'best',
+  '--batch-file',
+  batchFileLocation
 ];
-
+//if we are in production just dry run it
+if (!isProduction) {
+  args.push('--simulate');
+}
 console.log('spawning download process');
-const youtube_dl = spawn(`youtube-dl`, [...youtube_dl_options, playlistUrl]);
+const youtube_dl = spawn(`youtube-dl`, args);
 
 console.log('setting up event listeners');
 youtube_dl.stdout.on('data', (data) => console.log(`stdout: ${data}`));
